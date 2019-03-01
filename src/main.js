f23fbbf6c9193ef4bfb852db62f0ac60a5a703a2
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-03-01 16:27:34 +0900

    Tray Click Windows

--- src/main.js
+++ src/main.js
@@ -1,22 +1,37 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Tray } = require('electron');
 
-let mainWindow; 
-function createWindow() {
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.loadFile('index.html');
-    mainWindow.on('closed', () => {
-         mainWindow = null;
-    });
-}
-app.on('ready', createWindow);
+// function createWindow() {
+//     mainWindow = new BrowserWindow({width: 800, height: 600});
+//     mainWindow.loadFile('index.html');
+//     mainWindow.on('closed', () => {
+//          mainWindow = null;
+//     });
+// }
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on('ready',  () => {
+    let tray = new Tray(__dirname + '/images/icon2.png');
+    let win = new BrowserWindow({frame: false, width: 800, height: 600});
+    app.dock.hide();
+    win.loadFile('index.html');
+    tray.on('click', () => {
+        // win.isVisible() ? win.hide() : win.show()
+    })
+    win.on('show', () => {
+        tray.setHighlightMode('always')
+    })
+    win.on('hide', () => {
+        tray.setHighlightMode('never')
+    })
 });
-app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
-});
+
+
+// app.on('window-all-closed', () => {
+//     if (process.platform !== 'darwin') {
+//         app.quit();
+//     }
+// });
+// app.on('activate', () => {
+//     if (mainWindow === null) {
+//         createWindow();
+//     }
+// });
