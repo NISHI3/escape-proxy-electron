8f324b09515b36e461a0743163d53cae202c815c
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-03-19 05:23:50 +0900

    tsを消したので、削除

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,6 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// module.exports = {
-//     mode: 'development', // "production" | "development" | "none"
-//     target: 'electron-main',
-//     entry: path.join(__dirname, 'src', 'index'),
-//     cache: true,
-//     devtool: 'source-map',
-//     output: {
-//         filename: 'index.js',
-//         path: path.join(__dirname, 'dist')
-//     },
-//     module: {
-//         rules: [{
-//             test: /.ts?$/,
-//             include: [
-//                 path.resolve(__dirname, 'src'),
-//             ],
-//             exclude: [
-//                 path.resolve(__dirname, 'node_modules'),
-//             ],
-//             loader: 'ts-loader',
-//         }]
-//     },
-//     resolve: {
-//         extensions: [ '.ts', '.js' ]
-//     },
-// };
-
-
 const mainConfig = {
     target: 'electron-main',
     entry: './src/main.js',
