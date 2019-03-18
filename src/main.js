4bc730d803548d92b13fcd722c2a3015d132a935
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-03-19 05:13:25 +0900

    tray以外対応

--- src/main.js
+++ src/main.js
@@ -1,37 +1,54 @@
-const {app, BrowserWindow, Tray } = require('electron');
+// const {app, BrowserWindow, Tray } = require('electron');
+import { app, BrowserWindow, ipcMain } from 'electron';
+import { exec } from 'child_process';
 
-// function createWindow() {
-//     mainWindow = new BrowserWindow({width: 800, height: 600});
-//     mainWindow.loadFile('index.html');
-//     mainWindow.on('closed', () => {
-//          mainWindow = null;
-//     });
-// }
+let win
+function createWindow() {
+    // let tray = new Tray(__dirname + '/images/icon2.png');
+    // ToDo: BrowserWindowのプロパティで{x: 0, y: 0}があったので、ボタンの位置を取得して位置をいじる
+    win = new BrowserWindow({
+        frame: false,
+        width: 800,
+        height: 600
+    })
+    // app.dock.hide();
+    win.loadURL('file://' + __dirname + '/index.html')
+    // 開発時のみ
+    process.env.NODE_ENV === 'production' ? '' : win.webContents.openDevTools()
+    win.on('closed', () => {
+        win = null
+    })
+    // tray.on('click', () => {
+    //     win.isVisible() ? win.hide() : win.show()
+    // })
+    // win.on('show', () => {
+    //     tray.setHighlightMode('always')
+    // })
+    // win.on('hide', () => {
+    //     tray.setHighlightMode('never')
+    // })
+}
 
-app.on('ready',  () => {
-    let tray = new Tray(__dirname + '/images/icon2.png');
-    let win = new BrowserWindow({frame: false, width: 800, height: 600});
-    app.dock.hide();
-    win.loadFile('index.html');
-    tray.on('click', () => {
-        win.isVisible() ? win.hide() : win.show()
-    })
-    win.on('show', () => {
-        tray.setHighlightMode('always')
-    })
-    win.on('hide', () => {
-        tray.setHighlightMode('never')
-    })
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 
-// app.on('window-all-closed', () => {
-//     if (process.platform !== 'darwin') {
-//         app.quit();
-//     }
-// });
-// app.on('activate', () => {
-//     if (mainWindow === null) {
-//         createWindow();
-//     }
-// });
+  // 非同期(Main)
+  ipcMain.on('asynchronous-message', (event, arg) => {
+    console.log(arg)  // ping
+    exec('ls -la ./', (err, stdout, stderr) => {
+        if (err) { console.log(err); }
+        event.sender.send('asynchronous-reply', stdout)
+      });
+    // event.sender.send('asynchronous-reply', 'pong2')
+  })
