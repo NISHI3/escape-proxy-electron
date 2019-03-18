02b62540fc57c8f335fade70b9d453907b38076c
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-03-19 04:30:54 +0900

    MainProcessとRendererProcessに分割

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,75 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'development', // "production" | "development" | "none"
+// module.exports = {
+//     mode: 'development', // "production" | "development" | "none"
+//     target: 'electron-main',
+//     entry: path.join(__dirname, 'src', 'index'),
+//     cache: true,
+//     devtool: 'source-map',
+//     output: {
+//         filename: 'index.js',
+//         path: path.join(__dirname, 'dist')
+//     },
+//     module: {
+//         rules: [{
+//             test: /.ts?$/,
+//             include: [
+//                 path.resolve(__dirname, 'src'),
+//             ],
+//             exclude: [
+//                 path.resolve(__dirname, 'node_modules'),
+//             ],
+//             loader: 'ts-loader',
+//         }]
+//     },
+//     resolve: {
+//         extensions: [ '.ts', '.js' ]
+//     },
+// };
+
+
+const mainConfig = {
     target: 'electron-main',
-    entry: path.join(__dirname, 'src', 'index'),
-    cache: true,
-    devtool: 'source-map',
+    entry: './src/main.js',
     output: {
-        filename: 'index.js',
-        path: path.join(__dirname, 'dist')
+      path: path.join(__dirname, './src/dist'),
+      filename: 'main.js'
     },
+    // __dirnameを使えるように
+    node: {__dirname: false}
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.js$/,
+    //       exclude: /node_modules/,
+    //       loader: 'babel-loader'
+    //     }
+    //   ]
+    // }
+  }
+  
+  const rendererConfig = {
+    target: 'electron-renderer',
+    entry: './src/renderer.js',
+    output: {
+      path: path.join(__dirname, 'src/dist'),
+      filename: 'renderer.js'
+    },
+    node: {__dirname: false},
     module: {
-        rules: [{
-            test: /.ts?$/,
-            include: [
-                path.resolve(__dirname, 'src'),
-            ],
-            exclude: [
-                path.resolve(__dirname, 'node_modules'),
-            ],
-            loader: 'ts-loader',
-        }]
+      rules: []
     },
     resolve: {
-        extensions: [ '.ts', '.js' ]
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+      }
     },
-};
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        inject: 'body'
+  }),
+    ]
+  }
+  
+  module.exports = [ mainConfig, rendererConfig]
